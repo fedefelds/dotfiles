e6a2d138d317bb7e0035c425eaae300f4b0de2ba
Federico Feldsberg <fmfeldsberg@gmail.com>
2023-11-01 12:02:34 +0100

    hypr and startup.sh: change the special workspace from gui to a cli

--- .scripts/startup.sh
+++ .scripts/startup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 hyprctl dispatch exec '[workspace 1] kitty'
+
+#  hyprctl dispatch exec '[workspace specialworkspace silent] emacsclient --no-wait --create-frame --frame-parameters='\''(quote (name . "org-gtd"))'\'''
+hyprctl dispatch exec "kitty '--title' 'agenda' emacsclient -t"
