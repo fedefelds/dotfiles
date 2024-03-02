2529ffea469a1122837be1677daf9851491f41df
fedefelds <fedefelds@hotmail.com>
2024-03-03 00:42:08 +0100

    delete useless scripts

--- .scripts/startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-hyprctl dispatch exec '[workspace 1] kitty'
-
-#  hyprctl dispatch exec '[workspace specialworkspace silent] emacsclient --no-wait --create-frame --frame-parameters='\''(quote (name . "org-gtd"))'\'''
-hyprctl dispatch exec "kitty '--title' 'agenda' emacsclient -t"
