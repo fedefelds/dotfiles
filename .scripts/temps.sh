c9317486cc60ea5e97906e1a97ffa17e84f04fdd
fedefelds <fedefelds@hotmail.com>
2024-03-03 00:42:59 +0100

    Delete temps.sh

--- .scripts/temps.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-CPU_TEMP=$(sensors | grep Tctl | awk '{ print $2 }' | tr -d '+°C' | cut -d '.' -f 1)
-GPU_TEMP=$(sensors | grep GPU | awk '{ print $2 }' | tr -d '+°C' | cut -d '.' -f 1)
-
-echo " $CPU_TEMP $GPU_TEMP"
