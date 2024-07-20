38c23dbadf87e767a0cc82c58ecee161978033f5
Federico Martin Feldsberg <federicomartin.feldsberg@ims.co.at>
2024-07-19 20:55:51 -0700

    Create quick-capture.sh

---
+++ .scripts/quick-capture.sh
@@ -0,0 +1,13 @@
+#capture input from std in and transform it into an inbox-formated header
+#format:
+#  * this is a header
+#    [2024-07-16 Tue 13:23]
+
+while true; do
+    read -p "quick capture (empty line to exit): " line
+    if [ -z "$line" ]; then
+        break
+    fi
+    printf "* $line \n[$(date '+%Y-%m-%d %a %H:%M')]\n" | tee -a ~/Documents/org/gtd/inbox.org
+    echo                                                                                           
+done
